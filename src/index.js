20c2a6a1d2dd7296284cf688329bdd324e0d71e3
Ryan Morgan <RyanOnGithub@gmail.com>
2024-11-09 10:03:53 -0800

    Set up stylesheet and add basic outline for navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello, world!');
+import './styles.css';
+const logoRawSVG = require('svg-inline-loader?classPrefix!./images/winery-logo.svg');
+
+function renderBanner() {
+    const navbar = document.getElementById('nav-bar');
+
+    const leftBanner = document.createElement('div');
+    const rightBanner = document.createElement('div');
+    navbar.appendChild(leftBanner);
+    navbar.appendChild(rightBanner)
+
+    const logoContainer = document.createElement('div');
+    logoContainer.innerHTML = logoRawSVG;
+    leftBanner.appendChild(logoContainer);
+
+    const homeBtn = document.createElement('button');
+    homeBtn.id = 'home-button';
+    rightBanner.appendChild(homeBtn);
+
+    const winesBtn = document.createElement('button');
+    winesBtn.id = 'wines-button';
+    rightBanner.appendChild(winesBtn);
+
+
+    const aboutBtn = document.createElement('button');
+    aboutBtn.id = 'about-button';
+    rightBanner.appendChild(aboutBtn);
+}
+
+renderBanner();
+
