b5ab240704a1d07a9433b194550f49d7b1785f13
Ryan Morgan <RyanOnGithub@gmail.com>
2024-11-09 17:56:34 -0800

    Implement basic outline for wine list and about us pages

--- src/webpages/about-us.js
+++ src/webpages/about-us.js
@@ -1,11 +1,11 @@
 function renderAboutUs() {
     const body = document.getElementById('content');
     body.innerHTML = `
-        <p>
+        <p id='winery-about-us'>
             We are a family-owned and operated winery.
             Our roots run deep in the Willamette Valley
             and we believe in good ol' fashioned farm to 
-            table eating with wines that pair perfectly to
+            table eating with wines that pair perfectly with
             our local cuisine.
         </p>
     `;
--- src/webpages/winelist.js
+++ src/webpages/winelist.js
@@ -1,6 +1,17 @@
 function renderWineList() {
     const body = document.getElementById('content');
-    body.innerHTML = `<p>wine list</p>`;
+    body.innerHTML = `
+        <p id='wines-list'>
+            Our wines include:
+            <ul>
+                <li>Pinot Noir</li>
+                <li>Pinot Gris</li>
+                <li>Chardonnay</li>
+                <li>Granache</li>
+                <li>Riesling</li>
+            </ul>
+        </p>
+    `;
 }
 
 export default renderWineList;
