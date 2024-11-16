de78dd718729a56db3f61bcc32cb7a280e0f8fc2
Ryan Morgan <RyanOnGithub@gmail.com>
2024-11-16 10:06:47 -0800

    Add development and prod distinction to webpack files

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import renderHomepage from './webpages/home.js';
 renderNavbar();
 
 renderHomepage();
+
+/* Are we in a development or production environment? */
+console.log(process.env.NODE_ENV); 
